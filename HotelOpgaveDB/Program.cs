cd05c5284a119a30b5862cc2f7d7059779e58cbf
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2017-03-13 14:23:37 +0100

    Opgave 3: Join - List hotelnavn, adresse, samt værelsesinformation(nr, type, pris) om de værelser hotellerne har. - List alle de reservationer hver enkelt værelse har.

--- HotelOpgaveDB/Program.cs
+++ HotelOpgaveDB/Program.cs
@@ -12,6 +12,7 @@ namespace HotelOpgaveDB
         {
             using (var db = new HotelContext())
             {
+                //List alle informationer om alle hotellerne 
                 var ListAlleHoteller = from h in db.Hotel
                                        select h;
 
@@ -20,15 +21,48 @@ namespace HotelOpgaveDB
                     Console.WriteLine(item.Address + item.Hotel_No + item.Name);
                 }
 
-
-
+                //List alle informationer om alle kunderne
                 var ListAlleKunder = from g in db.Guest
                                      select g;
 
                 foreach (var item in ListAlleKunder)
                 {
-                    Console.WriteLine(item.Address + item.Guest_No + item.Name);
+                    Console.WriteLine(item.Name + item.Address + item.Guest_No);
                 }
+
+
+
+
+                //List hotelnavn, adresse, samt værelsesinformation(nr, type, pris) om de værelser hotellet har. 
+                var ListInfoVærelser = from h in db.Hotel
+                                       join r in db.Room
+                                       on h.Hotel_No equals r.Hotel_No
+                                       select new { h.Name, h.Address, r.Room_No, r.Types, r.Price};
+
+                foreach (var item in ListInfoVærelser)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+                //List alle de reservationer hver enkelt værelse har.
+                var ListAlleReservationer = from b in db.Booking
+                                            join r in db.Room
+                                            on b.Hotel_No equals r.Hotel_No
+                                            select new { r.Room_No, b.Booking_id };
+
+                foreach (var item in ListAlleReservationer)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+
+
+
+
+
+
 
 
 
